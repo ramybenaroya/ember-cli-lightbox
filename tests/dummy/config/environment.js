2e58eeeba1bafb936426d2512cc83936468c68b1
Ramy Ben Aroya <rami@dudamobile.com>
2014-10-03 05:39:39 +0300

    change basetag

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -40,7 +40,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-      ENV.baseURL = '/ember-cli-lightbox';
+      ENV.baseURL = '/ember-cli-lightbox/';
   }
 
   return ENV;
