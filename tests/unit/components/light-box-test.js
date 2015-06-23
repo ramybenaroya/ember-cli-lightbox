f581ffc011aaa4f50809ced9cdac4e0edf5a054d
ramyb <rami@dudamobile.com>
2015-06-23 10:24:20 +0300

    add unit test for component

--- tests/unit/components/light-box-test.js
+++ tests/unit/components/light-box-test.js
@@ -1,21 +1,54 @@
 import {
-  moduleForComponent,
-  test
-} from 'ember-qunit';
+	moduleForComponent,
+	test
+}
+from 'ember-qunit';
 
 import Ember from 'ember';
 
 moduleForComponent('light-box', 'LightBoxComponent', {
-  // specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+	// specify the other units that are required for this test
+	// needs: ['component:foo', 'helper:bar']
 });
 
 test('it renders', function() {
-  expect(2);
+	expect(2);
 
-  var component = this.subject();
-  equal(component._state, 'preRender');
+	var component = this.subject();
+	equal(component._state, 'preRender');
 
-  this.append();
-  equal(component._state, 'inDOM');
+	this.append();
+	equal(component._state, 'inDOM');
+});
+
+test('it opens lightbox on image click', function() {
+	expect(2);
+
+	var component = this.subject({
+		href: "assets/images/1.jpg",
+		"data-lightbox": "cat-1",
+		"data-title": "Cat 1"
+	});
+
+	this.append();
+
+	component.$().click();
+	equal($('#lightboxOverlay').length, 1);
+	equal($('#lightbox').length, 1);
+	
+});
+
+test('it renders a link without an image when `inlineImage` is false', function() {
+	expect(1);
+
+	var component = this.subject({
+		href: "assets/images/1.jpg",
+		"data-lightbox": "cat-1",
+		"data-title": "Cat 1",
+		inlineImage: false
+	});
+
+	this.append();
+
+	equal(this.$('img').length, 0);	
 });
